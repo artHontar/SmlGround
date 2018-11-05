2eaa93c37997177d3866e0fe32c1b75426b834f9
Artem Gontar <artemgontar16@gmail.com>
2018-11-05 15:34:24 +0300

    Add project files.

---
+++ SmlGround/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SmlGround
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SmlGround/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmlGround.MvcApplication" Language="C#" %>
