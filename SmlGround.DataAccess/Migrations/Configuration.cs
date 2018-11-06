7a5fdc7acf9002c89f16779e82cc51ff942dcb20
Artem Gontar <artemgontar16@gmail.com>
2018-11-06 09:34:12 +0300

    add migration

---
+++ SmlGround.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SmlGround.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SmlGround.DataAccess.Models.SocialDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SmlGround.DataAccess.Models.SocialDbContext";
+        }
+
+        protected override void Seed(SmlGround.DataAccess.Models.SocialDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
