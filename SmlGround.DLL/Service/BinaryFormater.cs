f48b3fd116dff36a00fca8fbb0f2cb8dbc565f29
Artem Gontar <artemgontar16@gmail.com>
2018-11-15 11:58:50 +0300

    add space

--- SmlGround.DLL/Service/BinaryFormater.cs
+++ SmlGround.DLL/Service/BinaryFormater.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-
-namespace SmlGround.DLL.Service
+﻿namespace SmlGround.DLL.Service
 {
     public class BinaryFormater
     {
         private string uploadImage;
+
         public BinaryFormater(string uploadImage)
         {
             this.uploadImage = uploadImage;
         }
+
         public byte[] Convert()
         {
             return System.Text.Encoding.Unicode.GetBytes(uploadImage);
